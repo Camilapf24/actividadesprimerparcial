52e192af4b622b0ff502999751a86198a63a8fe4
Camilapf24 <145036078+Camilapf24@users.noreply.github.com>
2023-10-17 09:18:43 -0600

    Create reto alimentos.py

---
+++ reto alimentos.py
@@ -0,0 +1,18 @@
+https://replit.com/@CamilaPerez37/retoalimentospy-2#main.py
+def evaluación_de_alimentos():
+  # Pedirle al usuario la cantidad de grasa de una porción de torta
+  torta = float(input("Ingresa la cantidad de grasa del pan en g"))
+
+  # Pedirle al usuario la cantidad de azúcar de un pan
+  azúcar = float(input("Ingresar la cantidad de azúcar por pan en g"))
+
+  # Pedirle al usuario la cantidad de sodio por pan
+  sodio = float(input("Ingresa la cantidad de sodio por pan en g"))
+
+  # Mostrar resultado
+  print("La cantidad de grasa es:", torta)
+  print("La cantidad de azúcar es:", azúcar)
+  print("La cantidad de sodio es:", sodio)
+
+# Call the function
+evaluación_de_alimentos()
